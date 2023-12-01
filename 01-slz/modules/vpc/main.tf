7213ef9d178baf966dac9661cd75bcfc3b19fcc0
asheeque <asheeque.cm@gmail.com>
2023-11-30 23:09:54 -0500

    vpc

--- 01-slz/modules/vpc/main.tf
+++ 01-slz/modules/vpc/main.tf
@@ -18,33 +18,33 @@ resource "aws_vpc" "main" {
   )
 }
 
-resource "aws_flow_log" "main" {
-  count = var.flow_logs ? 1 : 0
+# resource "aws_flow_log" "main" {
+#   count = var.flow_logs ? 1 : 0
 
-  log_destination      = aws_s3_bucket.vpc_flow_logs[0].arn
-  log_destination_type = "s3"
-  traffic_type         = "ALL"
-  vpc_id               = aws_vpc.main.id
+#   log_destination      = aws_s3_bucket.vpc_flow_logs[0].arn
+#   log_destination_type = "s3"
+#   traffic_type         = "ALL"
+#   vpc_id               = aws_vpc.main.id
 
-  tags = merge(
-    {
-      Component = local.flow_logs_component
-      Name      = join("-", [var.common_tags.Environment, var.common_tags.Product, local.flow_logs_component])
-    }
-  )
-}
+#   tags = merge(
+#     {
+#       Component = local.flow_logs_component
+#       Name      = join("-", [var.common_tags.Environment, var.common_tags.Product, local.flow_logs_component])
+#     }
+#   )
+# }
 
-resource "aws_s3_bucket" "vpc_flow_logs" {
-  count = var.flow_logs ? 1 : 0
+# resource "aws_s3_bucket" "vpc_flow_logs" {
+#   count = var.flow_logs ? 1 : 0
 
-  acl           = "private"
-  bucket        = "vpc-flow-logs-${aws_vpc.main.id}"
-  force_destroy = true
+#   acl           = "private"
+#   bucket        = "vpc-flow-logs-${aws_vpc.main.id}"
+#   force_destroy = true
 
-  tags = merge(
-    {
-      Component = local.s3_bucket_component
-      Name      = join("-", [var.common_tags.Environment, var.common_tags.Product, local.s3_bucket_component])
-    }
-  )
-}
+#   tags = merge(
+#     {
+#       Component = local.s3_bucket_component
+#       Name      = join("-", [var.common_tags.Environment, var.common_tags.Product, local.s3_bucket_component])
+#     }
+#   )
+# }
